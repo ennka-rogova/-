e0e5d4590dea78cce1e2a78f0f52b43864951775
Евгения <E.O.Rogova@outlook.com>
2020-11-18 13:26:13 +0700

    Задача на структуры и перечисления про монстров

---
+++ Monster/Monster.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+enum class MonsterType {
+    OGRE,
+    GOBLIN, 
+    SKELETON, 
+    ORC, 
+    TROLL
+};
+struct Monster {
+    MonsterType type;
+    std::string name;
+    int health;
+};
+std::string getMonsterType(Monster monster)
+{
+    if (monster.type == MonsterType::OGRE)
+        return "Ogre";
+    if (monster.type == MonsterType::GOBLIN)
+        return "Goblin";
+    if (monster.type == MonsterType::SKELETON)
+        return "Skeleton";
+    if (monster.type == MonsterType::ORC)
+        return "Orc";
+    if (monster.type == MonsterType::TROLL)
+        return "Troll";
+    return "Uknown";
+}
+void printMonster(Monster monster)
+{
+    std::cout << "This " << getMonsterType(monster);
+    std::cout << " is named " << monster.name << " and has " << monster.health << " health.\n";
+}
+int main()
+{
+    Monster goblin = { MonsterType::GOBLIN, "John", 170 };
+    Monster orc = { MonsterType::ORC, "James", 35 };
+    printMonster(goblin);
+    printMonster(orc);
+}
+
